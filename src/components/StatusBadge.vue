cabc1a1d9d11aabc9186d026601562ba31869a1e
MAC <sbenishwilliams@gmail.com>
2022-08-27 15:44:57 +0100

    created status badge component

---
+++ src/components/StatusBadge.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="status" :style="badgeStyle">
+    <p>{{status}}</p>
+  </div>
+</template>
+
+<script>
+import { computed } from '@vue/reactivity';
+
+export default {
+    props:{
+        status:{
+            type:String
+        }
+    },
+    setup(props){
+        const badgeStyle = computed(()=>{
+            if (props.status == 'paid'){
+                const style =  {'--badgeBackground': '#F4FDF9' ,'--badgeText': '#32D69F'}
+                return style
+            } else if (props.status == 'pending'){
+                const style =  {'--badgeBackground': '#FFF8F1' ,'--badgeText': '#FF8F00'}
+                return style
+            } else if (props.status == 'draft'){
+                const style =  {'--badgeBackground': '#F3F4F5' ,'--badgeText': '#373B53'}
+                return style
+            }
+        })
+
+        return{
+            badgeStyle
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .status{
+        border-radius: 5px;
+        background: var(--badgeBackground);
+        color:var(--badgeText);
+        display:flex;
+        justify-content: center;
+        align-items: center;
+        height:100%;
+        width:100px;
+    }
+    p::before{
+            content:"";
+            width:10px;
+            display: inline-block;
+            height:10px;
+            border-radius: 50%;
+            margin-right: 10px;
+            background: var(--badgeText);
+        }
+</style>
