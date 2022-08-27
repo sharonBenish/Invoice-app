d0debdc51713007fea1499c6ee7bd6380a944cb0
MAC <sbenishwilliams@gmail.com>
2022-08-27 15:41:40 +0100

    loaded data from json file

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,32 @@
 <template>
   <div>
     <InvoiceOptions />
+    <div class="invoice-list">
+      <InvoiceRecord v-for="(invoice, index) in invoiceData" :key="index" :invoice="invoice" />
+    </div>
   </div>
 </template>
 
 <script>
 import InvoiceOptions from "../components/InvoiceOptions.vue";
+import InvoiceRecord from "@/components/InvoiceRecord.vue";
+import json from '../data.json'
 export default {
-    components: { InvoiceOptions }
+    components: { InvoiceOptions, InvoiceRecord },
+    setup(){
+      const invoiceData = json
+      return{
+        invoiceData
+      }
+    }
 }
 </script>
 
-<style>
-
+<style scoped>
+.invoice-list{
+  display: flex;
+  flex-direction: column;
+  gap:1rem;
+  margin:1.5rem 0;
+}
 </style>
