ae6357f7a06c38d32f69c02538e42ffc904d684f
MAC <sbenishwilliams@gmail.com>
2022-09-02 22:43:57 +0100

    set up router

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import InvoiceInfo from '../views/InvoiceInfo.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path:'/invoice/:id',
+    name:'InvoiceInfo',
+    component:InvoiceInfo
   }
 ]
 
---
+++ src/views/InvoiceInfo.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div>{{id}}</div>
+        <div>{{info}}</div>
+    </div>
+</template>
+
+<script>
+import { ref } from '@vue/runtime-core';
+import { useRoute } from 'vue-router'
+export default {
+    setup(){
+        const route= useRoute()
+        const id = ref();
+        id.value = route.params.id;
+        const info = ref({});
+        info.value =JSON.parse(route.params.info);
+        return{
+            id,
+            info
+        }
+        
+
+    }
+}
+</script>
+
+<style>
+
+</style>
